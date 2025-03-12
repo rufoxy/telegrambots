0d3ad33eff6e9a473176af3c2be6f3e103c05dc3
rufoxy <karina.nikolaevs@yandex.ru>
2025-03-12 16:52:48 +1000

    #

--- app/handlers.py
+++ app/handlers.py
@@ -1,12 +1,41 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 import app.keyboards as kb
+from app.database.requests import set_user, get_item
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    await set_user(message.from_user.id)
     await message.answer('Добро пожаловать в магазин вязанных игрушек!',
                          reply_markup=kb.menu)
+
+
+@router.callback_query(F.data == 'start')
+async def callback_start(callback: CallbackQuery):
+    await callback.answer('Вы вернулись на главное меню')
+    await callback.message.edit_text('Добро пожаловать в магазин вязанных игрушек!',
+                                     reply_markup=kb.menu)
+
+
+@router.callback_query(F.data == 'catalog')
+async def catalog(callback: CallbackQuery):
+    await callback.answer('')
+    await callback.message.edit_text('Выберите категорию товара', reply_markup=await kb.categories())
+
+
+@router.callback_query(F.data.startswith('category_'))
+async def category(callback: CallbackQuery):
+    await callback.answer('')
+    await callback.message.edit_text('Выберите товар по категории',
+                                     reply_markup=await kb.get_items_by_categories(callback.data.split('_'[1])))
+
+
+@router.callback_query(F.data.startswith('item_'))
+async def item_handler(callback: CallbackQuery):
+    item = get_item(callback.data.split('_'[1]))
+    await callback.answer('')
+    await callback.message.edit_text(f'{item.name}\n\n{item.description}\n\nЦена: {item.price}', reply_markup=await kb.back_to_category(item.category))
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,6 +1,35 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from app.database.requests import get_categories, get_items_by_categories
 
 menu = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Каталог', callback_data='catalog')],
     [InlineKeyboardButton(text='Контакты', callback_data='contacts')]
 ])
+
+
+async def categories():
+    all_categories = get_categories()
+    keyboard = InlineKeyboardBuilder()
+    for category in all_categories:
+        keyboard.row(InlineKeyboardButton(text=category.name,
+                     callback_date=f'category_{category.id}'))
+    keyboard.row(InlineKeyboardButton(
+        text='На главную', callback_data='start'))
+    return keyboard.as_markup()
+
+
+async def items():
+    all_items = get_items_by_categories(category_id)
+    keyboard = InlineKeyboardBuilder
+    for item in all_items:
+        keyboard.row(InlineKeyboardButton(
+            text=item.name, callback_date=f'item_{item.id}'))
+    keyboard.row(InlineKeyboardButton(
+        text='К категориям', callback_date='catalog'))
+    return keyboard.as_markup()
+
+
+async def back_to_category():
+    InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
+        text='Назад', callback_data=f'category_{category_id}'),]])
